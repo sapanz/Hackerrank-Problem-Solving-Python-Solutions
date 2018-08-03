b0eb782d8b3ebfc9a6ce6e6e17d7c26c1c318e98
sapanz <spnzaveri@gmail.com>
2018-08-03 17:59:15 +0530

    Update Solve_Me_First.py

--- HackerRank-Solve Me First/Solve_Me_First.py
+++ HackerRank-Solve Me First/Solve_Me_First.py
@@ -1,7 +1,7 @@
 def solveMeFirst(a,b):
-	return a+b
+    return a+b
 
 num1 = int(input())
 num2 = int(input())
-res = solveMeFirst(num1,num2)
-print(res)
+value = solveMeFirst(num1,num2)
+print(value)
