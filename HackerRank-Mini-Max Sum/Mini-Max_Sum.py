62ebc0bf872ca28fcc25a68cdc1724092fb5809c
Yousseff Muhammed EL Masry <youssefmasry04@gmail.com>
2020-06-06 07:54:54 +0200

    Update Mini-Max_Sum.py

--- HackerRank-Mini-Max Sum/Mini-Max_Sum.py
+++ HackerRank-Mini-Max Sum/Mini-Max_Sum.py
@@ -8,12 +8,8 @@ import sys
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr):
-    sum=0
-    for i in range(len(arr)):
-        sum+=arr[i]
-    print ( sum-max(arr), sum-min(arr))
-
+    s=sum(arr)
+    print(s-max(arr),s-min(arr))
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
-
     miniMaxSum(arr)
